8965a99ac119f9b09642d679bc1c52ab9b9735e9
gameschool-student1 <stud1.gschl24@outlook.com>
2025-04-09 18:56:35 +0700

    09. Структуры читай массивы

--- Salat/Salat.cpp
+++ Salat/Salat.cpp
@@ -1,75 +1,100 @@
-﻿// if (user == 1 && bot == 2 || user == 2 && bot == 3 || user == 3 && bot == 1) {
-    //   std::cout << "win";
-    //}
-     //if (user < 3 && user > 1)
-     //{
-        // std::cout << "no";
-     //}
-     //(1 && 2 || 2 && 3 || 3 && 1)
-//if (user == bot) {
-//std::cout << "draw\n";    
-//  }
- //else if ((user == 1 && bot == 2) || (user == 2 && bot == 3) || (user == 3 && bot == 1)) {
-   //  std::cout << "win\n";
-       // }
- //else {
-    // std::cout << "no win\n";
-       // }
+﻿//        switch (bot) {
+    //
+//            case 1: 
+//                if (user == 1) {
+//                    std::cout << "draw\n";
+//                }
+//                if (user == 2) {
+//                    i++;
+//                    std::cout << "win\n";
+//                }
+//                if (user == 3) {
+//                    std::cout << "no win\n";
+//                }
+//                break;
+//           
+//
+//            case 2: 
+//                if (user == 2) {
+//                    std::cout << "draw\n";
+//                }
+//                if (user == 3) {
+//                    std::cout << "win\n";
+//                    i++;
+//                }
+//                if (user == 1) {
+//                    std::cout << "no win\n";
+//                }
+//                break;
+//            
+
+           /* case 3: 
+                if (user == 3) {
+                    std::cout << "draw\n";
+                }
+                if (user == 1) {
+                    std::cout << "win\n";
+                    i++;
+                }
+                if (user == 2) {
+                    std::cout << "no win\n";
+                }
+                break;
+        
+
+                  std::cout << "replay?\n";
+
+            default:
+                if (user != 1 || user != 2 || user != 3) {
+                    std::cout << "ne verno\n";
+                }
+                break;
+        }*/
+
+struct object {
+    int x, y;
+};
+
+struct items {
+
+    int compas;
+    int watch;
+    bool phone = false;
+    object heal;
+};
 
 #include <iostream>
-    setlocale(LC_ALL, "Russian");
+void game() {
 
-int main() {
-    int bot = 3;
     int user = 1;
 
     std::cout << "1.Камень\n2.Ножницы\n3.Бумага\n";
+    
+   
+    for (int i = 0; i < 3;) {
 
-    do { 
-        std::cin >> user; 
-    } 
-    while (user > 1 && user < 3) {
-        switch (user) {
-        case 1:{
-            if (user == 1) {
-                std::cout << "draw\n";
-            }
-            if (user == 2) {
-                std::cout << "win\n";
-            }
-            if (user == 3) {
-                std::cout << "no win\n";
-            }
-            break;
-        }
-        case 2:{
-            if (user == 2) {
-                std::cout << "draw\n";
-            }
-            if (user == 3) {
-                std::cout << "win\n";
-            }
-            if (user == 1) {
-                std::cout << "no win\n";
-            }
-            break;
-        }
-          case 3: {
-              if (user == 3) {
-                  std::cout << "draw\n";
-              }
-              if (user == 1) {
-                  std::cout << "win\n";
-              }
-              if (user == 2) {
-                  std::cout << "no win\n";
-              }
-              break;
-          }
-            std::cout << "replay?\n";
-           default:
-               if (user != 1 || user != 2 || user != 3) {
-                   std::cout << "ne verno\n";
-               }
-               break;
-        }
+        std::cin >> user;
+
+    int bot = 1 + rand() % 3;
+
+         if (user == 1 && bot == 2 || user == 2 && bot == 3 || user == 3 && bot == 1) {
+               std::cout << "win\n";
+               i++;
+         }
+
+         else if (user == bot) {
+             std::cout << "draw\n";
+         }
+
+         else {
+             std::cout << "louse\n";
+         }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    game();
+}
