a879bc4d6559b075ab0577b3d605fbe26ed47bfa
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-04-11 17:18:39 +0700

    11.04. написать дальше, с использованием структур

--- Salat/Salat.cpp
+++ Salat/Salat.cpp
@@ -1,4 +1,7 @@
-﻿//        switch (bot) {
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+//        switch (bot) {
     //
 //            case 1: 
 //                if (user == 1) {
@@ -63,38 +66,122 @@ struct items {
     object heal;
 };
 
-#include <iostream>
-void game() {
+struct info {
+    int hp, attack;
 
-    int user = 1;
+    items p;
+    std::string name;
+    std::string ch;
 
-    std::cout << "1.Камень\n2.Ножницы\n3.Бумага\n";
+};
+
+std::vector<items>z;
+
+class p {
+    int vedro;
+
+public:
+    int abs;
+
+    void game() {
+
+
+        std::cout << "1.Камень\n2.Ножницы\n3.Бумага\n";
+
+        std::string user;
+
+        std::string bot[3]{ "камень","ножницы","бумага" };
+
+        for (int i = 0; i < 3;) {
+
+            std::cin >> user;
+            int temp = 1 + rand() % 3;
+
+            if (user == "камень" && bot[temp] == "ножницы" || user == "ножницы" && bot[temp] == "бумага" || user == "бумага" && bot[temp] == "камень") {
+                std::cout << "win\n";
+                i++;
+            }
+            else if (user == bot[temp]) {
+                std::cout << "draw\n";
+            }
+            else {
+                std::cout << "louse\n";
+            }
+        }
+    }
+
+};
+
+items a;
+p x;
+
+info user;
+void dom() {
     
-   
-    for (int i = 0; i < 3;) {
+    int numbers[4] { 1, -5, -8, 6 };
+    int n = sizeof(numbers) / sizeof(numbers[0]);
 
-        std::cin >> user;
+    std::cout << "Вывод чисел:\n";
+    for (int i = 0; i < n;) {
+        std::cout << numbers[i] << std::endl;
+        i++;
+    }
 
-    int bot = 1 + rand() % 3;
+    std::cout << "положительные числа:\n";
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] > 0) {
+            std::cout << numbers[i] << std::endl;
+        }
+    }
 
-         if (user == 1 && bot == 2 || user == 2 && bot == 3 || user == 3 && bot == 1) {
-               std::cout << "win\n";
-               i++;
-         }
+    std::cout << "отрицательные числа:\n";
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] < 0) {
+            std::cout << numbers[i] << std::endl;
+        }
+    }
 
-         else if (user == bot) {
-             std::cout << "draw\n";
-         }
+    std::cout << "В порядке увеличения:\n";
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j > n; j++) {
 
-         else {
-             std::cout << "louse\n";
-         }
+            if (numbers[j] > numbers[j + 1]) {
+                int temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+            std::cout << numbers[i] << std::endl;
     }
 }
 
+
+
+   /* for (int i = 0;i < n; i++) {
+    std::cout << numbers[i] << std::endl;
+    }*/ //и чо за ошибка аааааааа
+
+void InitGame() {
+    std::cout << "Ты ведь хотел когда нибудь оказаться внутри себя? Узнать кем являешься со стороны, каким бы мог быть, каким был на самом деле и какому я тебя ведёт твой путь! Теперь я помогу тебе. Куда ты хочешь пойти в первую очередь?";
+    std::cout << "1.Child\n 2. home\n";
+        std::cin >> user.ch;
+        
+        if (user.ch == "Child") {
+            x.game();
+        }
+        else if (user.ch == "home") {
+            dom();
+        }
+        else {
+            std::cout << "ERROR";
+        }
+}
+
+
 int main() {
     setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
     srand(time(NULL));
+    InitGame();
 
-    game();
 }
